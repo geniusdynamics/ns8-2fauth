2f9f523e04f676f5199e2d58e18b33de7da04836
Mr Genius <martin@genius.ke>
2026-09-21 11:37:30 +0300

    Update app version from 6.0.0 to 8.0.2

--- build-images.sh
+++ build-images.sh
@@ -14,7 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="2fauth"
-app_version="6.0.0"
+app_version="8.0.2"
 # Create a new empty container image
 container=$(buildah from scratch)
 
